cbbb9ea1a980a2a1b8737334fa46538040313a99
Daviid0819 <szucs.david1908@gmail.com>
2023-01-21 23:58:09 +0100

    Repush

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -57,7 +57,7 @@ const Login = () => {
 
     const handleVerify = async() => {
         if(formData.username === ""){
-            return setErr("Enter your username");
+            return setErr("Enter your username, please");
         }
 
         setLoading(true);
